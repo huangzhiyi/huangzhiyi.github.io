01237fba79823d4fd2bd92d82d3417551ca5e33c
huangzhiyi <huangzhiyi@gmail.com>
2021-08-30 01:05:32 +0800

    新增导航链接和复制提示功能

--- js/codecopy.bundle.js
+++ js/codecopy.bundle.js
@@ -5,22 +5,22 @@ const codecopys = document.getElementsByTagName('pre');
 for (let i = 0; i < codecopys.length; i++) {
     //pre元素对象
     let codecopy = codecopys[i];
- 
+
     //生成复制按钮
     let html_temp = '<div class="codecopy-btn" id="copybtn_'+i+'" data-title="复制" data-clipboard-action="copy" data-clipboard-target="#code_' + i + '"' +
         'οnclick="onC(this);" οnmοuseοut="mOut(this)">复制</div>';
 
-	codecopy.setAttribute('class','codecopy');
+	  codecopy.setAttribute('class','codecopy');
     codecopy.firstChild.setAttribute('id', 'code_' + i);
- 
+
     //将复制按钮追加至页面
     let html = codecopy.innerHTML + html_temp;
- 
+
     codecopy.innerHTML = html;
 }
 /*初始化复制功能*/
 const clipboardJs = new ClipboardJS('.codecopy-btn');//注意：ClipboardJS替代了Clipboard
- 
+
 /*复制成功事件*/
 clipboardJs.on('success', function (e) {
 	e.trigger.innerHTML = '复制成功';
